2ef15deaf453905c63e748524f7b31cdb91c157b
mafalda.andre <mafalda.andre@proside.pt>
2024-04-10 12:07:39 +0100

    Initial commit

---
+++ src/pages/initialPage/InitialPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InitialPage = () => {
+  return (
+    <div>
+      <h1>Olá,</h1>
+      <h2>chamo-me MAFALDA ANDRÉ</h2>
+      <h3>E sou FRONT END DEVELOPER E UI/UX DESIGNER</h3>
+    </div>
+  );
+};
+
+export default InitialPage;
